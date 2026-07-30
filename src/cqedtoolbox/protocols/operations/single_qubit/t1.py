44aae60070061cca73176fed7421d34cca7a4307
al100-aleo <al100-aleo@users.noreply.github.com>
2026-07-30 11:27:37 -0500

    adjusted t1 synthetic data generation to match eqn 53 by including an offset term, similar to the fixes for t2r and t2e

--- src/cqedtoolbox/protocols/operations/single_qubit/t1.py
+++ src/cqedtoolbox/protocols/operations/single_qubit/t1.py
@@ -195,8 +195,9 @@ class IncreaseAveragingCorrection(Correction):
 class T1Operation(ProtocolOperation):
 
     _SIM_T1 = 20.0
-    _SIM_AMP = 0.5
+    _SIM_AMP = 0.35 + 0.35j
     _SIM_NOISE_AMP = 0.02
+    _SIM_OFFSET = 0.4 + 0.4j
 
     def __init__(self, params):
         super().__init__()
@@ -261,7 +262,7 @@ class T1Operation(ProtocolOperation):
         logger.info("Starting dummy T1 measurement")
         delays = np.linspace(0, 5 * self._SIM_T1, int(self.steps()))
         signal_gen = lambda delays: (self._SIM_AMP * np.exp(-delays / self._SIM_T1)
-                  + self._SIM_NOISE_AMP * (np.random.randn() + 1j * np.random.randn()))
+                  + self._SIM_OFFSET + self._SIM_NOISE_AMP * (np.random.randn() + 1j * np.random.randn()))
         sweep = sweep_parameter("delays", delays, record_as(signal_gen, "signal"))
         loc, _ = run_and_save_sweep(sweep, "data", self.name)
         logger.info("Dummy measurement complete")
